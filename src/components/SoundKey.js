0806bd1a4778986280279c8b50997331c59a0766
Ifeanyi Orji <chrisifwax101@gmail.com>
2019-02-27 14:01:21 +0100

    Added music assets Setup the keylistener and added an array of keycodes and the corresponding sounds they should make Set up the SoundKey component that holds the click logic and the key views

--- src/components/SoundKey.js
+++ src/components/SoundKey.js
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 import '../styles/sound-key.css';
 
 class SoundKey extends Component {
+    constructor(props) {
+        super(props);
+
+        this.keyRef = React.createRef();
+    }
+    componentDidMount() {
+        const keyCode = this.props.keyCode;
+        const parent = this;
+        window.addEventListener('keydown', function(e) {
+            if (e.keyCode === parseInt(keyCode)){
+                parent.playSound();
+            }
+        });
+
+        this.keyRef.current.addEventListener('transitionend', this.removeTransition);
+    }
+
+    playSound = () => {
+        this.keyRef.current.classList.add('playing');
+        const audio = new Audio(this.props.soundFile);
+        audio.currentTime = 0;
+        audio.play();
+    };
+
+    removeTransition = () => {
+        this.keyRef.current.classList.remove('playing');
+    };
 
     render() {
-        const {props} = this.props;
+        const props = this.props;
         return (
-            <div data-key={props.dataKey} className="key">
-                <kbd>{props.keyValue}</kbd>
-                <span className="sound">{props.soundName}</span>
+            <div data-key={props.keyCode} className="key" ref={this.keyRef}>
+                <div>
+                    <kbd>{props.keyValue}</kbd>
+                    <span className="sound">{props.soundName}</span>
+                </div>
             </div>
         );
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import SoundKey from '../components/SoundKey';
+import sounds from '../helpers/sounds';
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
-
+          {sounds.map((sound, idx) => {
+              return (
+                  <SoundKey
+                      key={idx}
+                      keyCode={sound.keyCode}
+                      keyValue={sound.keyValue}
+                      soundName={sound.soundName}
+                      soundFile={`../` + sound.soundFile} />
+              );
+          })}
       </div>
     );
   }
